4ddebc3ac014b0873540d546b6005cea09eeafcf
AhteshamUddowla <ahteshamiiuc@gmail.com>
2024-04-13 15:23:51 +0600

    image-field

--- leads/models.py
+++ leads/models.py
@@ -28,9 +28,10 @@ class Lead(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
     phone_number = models.CharField(max_length=20)
     email = models.EmailField()
-
-    objects = LeadManager()
+    profile_picture = models.ImageField(null=True, blank=True, upload_to="profile_pictures/")
     
+    objects = LeadManager()
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
     
